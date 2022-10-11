cc21bd1f50e80c7a8e5be0470d18f834af740c47
so0112 <tkddh0112@gmail.com>
2022-10-11 18:59:56 +0900

    Feat: 회원가입 유효성 검사

---
+++ src/function/checkSignup.js
@@ -0,0 +1,16 @@
+/** 이메일 정합성 체크 함수 */
+export function checkEmail(email) {
+  let emailReg =
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return emailReg.test(email);
+}
+
+/** 비밀번호 정합성 체크 함수 */
+export function checkPassword(password) {
+  return password.length >= 8;
+}
+
+/** 비밀번호 확인 정합성 체크 함수 */
+export function cofirmPassword(password, password2) {
+  return password === password2;
+}
