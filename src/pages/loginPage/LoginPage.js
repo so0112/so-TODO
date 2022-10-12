2bc570fd3de51820e1fdeaf583848452b7fee761
so0112 <tkddh0112@gmail.com>
2022-10-12 23:19:53 +0900

    Chore: 회원가입 로그인 단순 수정

--- src/pages/loginPage/LoginPage.js
+++ src/pages/loginPage/LoginPage.js
@@ -1,7 +1,14 @@
 import styled from "styled-components";
 import Login from "../../components/login/Login";
 
+const isLogin = Boolean(localStorage.getItem("token"));
+
 function LoginPage() {
+  // 토큰 유무 확인
+  if (isLogin) {
+    window.location.replace("/todo");
+  }
+
   return (
     <LoginLayout>
       <Login />
