6b6678f9ca56e4f83579a9e74b743320ef586ccf
so0112 <tkddh0112@gmail.com>
2022-10-13 01:29:14 +0900

    Modify: api 함수명 변경

---
+++ src/api/axiosLogin.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+/** 로그인 POST API
+ * handleLogin(URL, 아이디, 비밀번호, dispatch)
+ */
+
+export const postLogin = async (LOGIN_URL, email, password) => {
+  try {
+    const res = await axios.post(
+      LOGIN_URL,
+      {
+        email,
+        password,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    console.log("res : ", res);
+    localStorage.setItem("token", res.data["access_token"]);
+    window.location.replace("/todo");
+  } catch (error) {
+    console.log(error);
+  }
+};
