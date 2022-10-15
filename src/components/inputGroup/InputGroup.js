5ea13527ecbe1cb87632531d3f63cb337601e50a
so0112 <tkddh0112@gmail.com>
2022-10-16 02:11:54 +0900

    Fix: setIsError 오류

--- src/components/inputGroup/InputGroup.js
+++ src/components/inputGroup/InputGroup.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const InputGroup = ({ type = "text", placeholder = "", value, setValue, setIsError = true }) => {
+const InputGroup = ({ type = "text", placeholder = "", value, setValue, setIsError = Boolean }) => {
   return (
     <div>
       <input
